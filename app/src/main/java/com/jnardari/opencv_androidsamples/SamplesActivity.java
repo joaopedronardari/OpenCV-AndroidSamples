a47664a35d87069f4e9da903d02c60d7b58984fb
Joao Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-08-24 13:59:19 -0300

    First commit...

---
+++ app/src/main/java/com/jnardari/opencv_androidsamples/SamplesActivity.java
@@ -0,0 +1,15 @@
+package com.jnardari.opencv_androidsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SamplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samples);
+    }
+}
