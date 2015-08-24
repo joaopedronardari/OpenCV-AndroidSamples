9e1dc5e34cf27a682a5f812c28c0a6b30e98aeee
Joao Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-08-24 14:40:23 -0300

    OpenCv Integrated

---
+++ app/src/main/java/com/jnardari/opencv_androidsamples/activities/SamplesActivity.java
@@ -0,0 +1,50 @@
+package com.jnardari.opencv_androidsamples.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.jnardari.opencv_androidsamples.R;
+
+public class SamplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samples);
+    }
+
+    public void tutorial1(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void tutorial2(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void tutorial3(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void imageManipulations(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void faceDetection(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void colorBlobDetection(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void cameraCalibration(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+
+    public void puzzle15(View v) {
+        // TODO - Create Intent and Start Activity
+    }
+}
