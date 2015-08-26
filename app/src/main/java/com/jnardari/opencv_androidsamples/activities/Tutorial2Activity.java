be2e2f4b553e6886a731af7580d0a987d1c9841c
Joao Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-08-26 15:06:23 -0300

    Removing unused menu.xml's

--- app/src/main/java/com/jnardari/opencv_androidsamples/activities/Tutorial2Activity.java
+++ app/src/main/java/com/jnardari/opencv_androidsamples/activities/Tutorial2Activity.java
@@ -14,26 +14,4 @@ public class Tutorial2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial2);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutorial2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
